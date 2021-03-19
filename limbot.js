76cd478da8826db9253adf3b9fc6a82633c021e3
ouyangzhaoxing <ouyang-zhaoxing@qq.com>
2021-03-20 01:28:20 +0800

    [add] join tips

--- limbot.js
+++ limbot.js
@@ -16,6 +16,37 @@ jsonfile.readFile("./config.json", function (err, config) {
     process.stdin.once("data", () => bot.login()); // 设备锁验证
   });
 
+  bot.on("notice.group.increase", (data) => { //监听群成员加入
+
+    if (!checkListen(data.group_id)) return;
+
+    joinTips(data);
+
+  });
+
   bot.login(config.password); // 登录
+
+  /*-------------------------------------------------------------------------*/
+
+  /** 检查群组是否需要监听 */
+  function checkListen(group_id) {
+
+    for (let index = 0; index < config.listen.length; index++) {
+      if (config.listen[index] == group_id) return true;
+    }
+
+    return false;
+
+  }
+
+  /** 入群提示 */
+  function joinTips(data) {
+
+    if (!config.function.join_tips) return;
+
+    let tips = config.tipsTemplate.join.replace("[nickname]", "[CQ:at,qq=" + data.user_id + "]");
+    bot.sendGroupMsg(data.group_id, tips);
+
+  }
 
 });
